ad2881a28df663c2657db3f879051c67d8b9f3d5
sukjong2002 <sukjong2002@gmail.com>
2017-11-07 22:11:03 -0500

    add korean translate

---
+++ core/src/plugins/access.ajxp_home/res/i18n/kr.php
@@ -0,0 +1,157 @@
+<?php
+/*
+ * Copyright 2007-2017 Charles du Jeu - Abstrium SAS <team (at) pyd.io>
+ * This file is part of Pydio.
+ *
+ * Pydio is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Pydio is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Pydio.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * The latest code can be found at <https://pydio.com>.
+ */
+$mess=array(
+"title"=> "홈",
+"desc" => "환영 페이지",
+"39"=> "Below is a list of the workspaces you can access to. They can be personal or shared with/by others users.",
+"40"=> "어서오세요, %s! ",
+"41"=> "이 작업 공간을 기본으로 설정합니다",
+"42"=> "Enter",
+"43"=> "내 프로필",
+"43t"=> "모든 개인 정보",
+"44"=> "내 계정",
+"45"=> "개인 정보",
+"46"=> "주소록",
+"47"=> "당신이 만든 사용자 게약서 입니다.",
+"48"=> "My Users",
+"49"=> "External users that you created",
+"50"=> "My Teams",
+"51"=> "Teams of users, used as shortcuts when sharing",
+"52"=> "진짜로 이 팀을 삭제할까요? 이 작업은 사용자를 삭제할수 없습니다.",
+"53"=> "시작하기",
+"54"=> "Get started with APPLICATION_TITLE by following How-to videos",
+"55"=> "APPLICATION_TITLE에 새로오셨나요? <a>튜토리얼을 시청하세요</a>!",
+"56"=> "Getting started with APPLICATION_TITLE? Here are some videos for you!",
+"57"=> "APPLICATION_TITLE을 다운로드하세요...",
+"58"=> "안드로이드",
+"59"=> "아이폰/아이패드",
+    "60"=>"맥 OS",
+    "61"=>"윈도우즈",
+
+    "62"=> "<h2>[입문서] 작업 공간이 무엇인가요?</h2>
+            이 비디오는 APPLICATION_TITLE 의 작업공간이 무엇인지 설명합니다.
+            <ul>
+                <li>Why are Workspaces central in APPLICATION_TITLE?</li>
+                <li>What are Workspaces from a technical perspective?</li>
+                <li>How do they allow you to organise your data?</li>
+            </ul>
+            ",
+
+    "63"=> "<h2>[How-to] Upload files to APPLICATION_TITLE</h2>
+            Get started with APPLICATION_TITLE and see how to upload files
+            <ul>
+                <li>Drag and drop files into APPLICATION_TITLE</li>
+                <li>Create a new folder</li>
+                <li>Rename files and folders</li>
+                <li>Download files to your Computer</li>
+            </ul>
+            ",
+
+    "64"=> "<h2>[입문서] 당신의 파일을 검색하고, 여세요</h2>
+            This video shows you various tips to efficiently manage your files in APPLICATION_TITLE
+            <ul>
+                <li>Cross-workspaces search bar</li>
+                <li>Content display modes, Keyboard shortcuts</li>
+                <li>Sort Files</li>
+                <li>Meta Data, Bookmarks</li>
+            </ul>
+            ",
+
+    "79"=> "<h2>[입문서] 파일과 폴더를 공유하기</h2>
+            This tutorial shows you how to share files and folders, internally or externally
+            <ul>
+                <li>Creating share links for external people</li>
+                <li>Sharing files and folders with internal collaborators</li>
+                <li>Sharing a folder as a workspace</li>
+                <li>Layout options of shared folders</li>
+                <li>Creating a blind drop folder</li>
+                <li>Managing users and share rights</li>
+            </ul>
+            ",
+
+    "80"=> "<h2>[How-to] Sharing : security features</h2>
+            This video goes through the various share settings that help you secure your shares
+            <ul>
+                <li>Password protection</li>
+                <li>Expiration date</li>
+                <li>Limit the number of downloads</li>
+            </ul>
+            ",
+
+    "81"=> "<h2>[How-to] Customize your share links</h2>
+            See how you can customise the URL of your share links to make them look more user-friendly.
+            ",
+
+    "82"=> "<h2>[How-to] Shared links custom rights</h2>
+            This video shows you how to manage the permissions attached to your shares
+            <ul>
+                <li>Preview</li>
+                <li>Download</li>
+                <li>Upload / Edit</li>
+            </ul>
+            ",
+
+    "83"=> "<h2>[How-to] Language</h2>
+            APPLICATION_TITLE is available in 28 languages. This video shows you how to change the language in APPLICATION_TITLE.
+            ",
+
+    "84"=> "<h2>[How-to] Alerts</h2>
+            Learn how to set alerts on a file, in order to see when it has been modified or consulted.
+            ",
+
+    "85"=> "<h2>[How-to] Collaborative edition</h2>
+            This video shows you how to collaborate on office docs, thanks to Collabora online office suite
+            <ul>
+                <li>Work on text docs, presentations, spreadsheets</li>
+                <li>Edit docs collaboratively in real time</li>
+                <li>Create new documents directly from your browser</li>
+            </ul>
+            ",
+    "65"=> "Discover more on the APPLICATION_TITLE channel!",
+    "66"=>"<h2>[How-to] Using APPLICATION_TITLE on iOS device</h2>
+                            This video explains how to use the APPLICATION_TITLE application (available on the App Store). Process is quite similar on Android as well.
+<ul><li>Setup the server connection</li>
+<li>Navigating through the files</li>
+<li>Keeping files offline and interacting with external applications</li>
+<li>Protecting the app with a PIN-code</li>
+<li>Using bookmarks and search engine</li></ul>",
+"67"=> "If you are not %s, %logout.",
+"68"=> "윈도우즈용 동기화 클라이언트 다운로드",
+"69"=> "맥 OSX용 동기화 클라이언트 다운로드",
+"70"=> "iOS 기기용 네이티브 애플리케이션",
+"71"=> "안드로이드 기기용 네이티브 애플리케이션",
+"72" => "서버 QR코드",
+"73" => "쉽게 모바일 애플리케이션과 연결할 수 있습니다.",
+"74" => "이 QR코드를 사용하여 쉽게 모바일 애플리케이션을 구성할 수 있습니다.",
+"75"=> "모든 파일 검색중...",
+"76" => "Access the %1 through the top-right menu",
+"77" => "관리자 대시보드",
+"78" => "Pick a target workspace",
+    "86" => "동영상 재생",
+    "87" => "최근 기록",
+    "88" => "데스크탑에서 여기로 끌어놓으세요",
+    "89" => "파일을 선택해 업로드하세요",
+    "90" => "Select target workspace",
+    "91" => "Keep your files offline with our Desktop Sync Client",
+    "92" => "애플리케이션 다운로드",
+    "93" => "빠른 업로드",
+    "94" => "비디오 튜토리얼",
+);
